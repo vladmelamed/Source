767acc88366aa0ec5a1e9432b442f9d646931a60
vlad melamed <melamedvlad@gmail.com>
2020-03-05 10:29:51 -0500

    editview fixes: unieq ip

--- public/javascripts/views/EditView.js
+++ public/javascripts/views/EditView.js
@@ -8,6 +8,14 @@ var EditView = Backbone.View.extend({
     initialize: function () {
         _.bindAll(this);
 
+        var self = this;
+        self.ipList = [];
+        $.get('/ips', {}, function(result){
+            self.ipList = result;
+        });
+        this.myIP = "";
+
+
         this.$ip = $("#editForm [name='ip']");
         this.$ip.mask('099.099.099.099');
         this.$name = $("#editForm [name='name']");
@@ -21,24 +29,33 @@ var EditView = Backbone.View.extend({
     },
     render: function () {
         this.$ip.val(this.model.get("ip"));
+        this.myIP = this.model.get("ip"); // for edits
         this.$name.val(this.model.get("name"));
         this.$domain.val(this.model.get("domain"));
         this.$username.val(this.model.get("username"));
         this.$password.val(this.model.get("password"));
-        this.$addDate.val(this.model.get("addDate"));
-        this.$updateDate.val(this.model.get("updateDate"));
+        this.$addDate.val(this.model.get("addDate").toLocaleDateString());
+        this.$updateDate.val(this.model.get("updateDate").toLocaleDateString());
         this.$tags.val(this.model.get("tags"));
         this.$el.show();
         window.scrollTo(0,0);
     },
     onSave: function () {
         var _this = this;
-        if (this.$ip.val()=="" || this.$name.val()=="") {
-            alert("Name and IP address can't be empty")
+        if (this.$ip.val().trim()=="" || this.$name.val().trim()=="") {
+            alert("Name and IP address can't be empty");
             return;
         }
+        if (!(this.myIP==this.$ip.val())) {  //new ip
+            var iip = this.$ip.val().trim();
+            if (this.ipList.indexOf(iip)>-1){
+                alert("IP address should be unique. You entered the existing address.");
+                return;
+            }
+        }
+
         if (this.$password.val()=="" || this.$password1.val()=="" || (!(this.$password.val() == this.$password1.val()))) {
-            alert("Please retype the password second time")
+            alert("Please retype the password second time");
             return;
         }
         this.model.save(
@@ -48,8 +65,7 @@ var EditView = Backbone.View.extend({
                 domain:this.$domain.val(),
                 username:this.$username.val(),
                 password:this.$password.val(),
-                addDate:this.$addDate.val(),
-                updateDate:this.$updateDate.val(),
+                updateDate: new Date(),  //last update data
                 tags:this.$tags.val()
             }
         ).done(function () {
